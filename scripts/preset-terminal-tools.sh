38667083e14d1452c2c1d742d47c64e27010e8ec
GuoDong Xu <65591330+xugd2019@users.noreply.github.com>
2025-01-02 19:38:02 +0800

    commit

---
+++ scripts/preset-terminal-tools.sh
@@ -0,0 +1,7 @@
+mkdir -p files/root
+
+git clone https://github.com/robbyrussell/oh-my-zsh ./root/.oh-my-zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git ./.oh-my-zsh/custom//themes/powerlevel10k
