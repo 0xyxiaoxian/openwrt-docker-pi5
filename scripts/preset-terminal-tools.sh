9a25adc9ece01ff93d5b5efba766efdc7f87cb80
xugd <xugd2019@users.noreply.github.com>
2025-01-06 20:44:11 +0800

    zsh安装未生效修复

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,6 +1,7 @@
 mkdir -p files/root
+pushd files/root
 
-git clone https://github.com/robbyrussell/oh-my-zsh ./root/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
